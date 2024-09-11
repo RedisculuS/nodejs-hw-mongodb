2f0df62ea4d449acb783f4224003336d1fb6d84e
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2024-09-11 17:47:16 +0300

    2nd commit

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import { env } from './utils/env.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
 
-const PORT = Number(env('PORT', '3001'));
+const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
