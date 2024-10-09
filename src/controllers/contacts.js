798171e07792cead68d4032caad290f77f278281
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2024-10-09 20:40:03 +0300

    tasks finished

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -11,6 +11,9 @@ import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
 
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
+
 export const getContactsController = async (req, res, next) => {
   const { page, perPage } = parsePaginationParams(req.query);
   const { sortBy, sortOrder } = parseSortParams(req.query);
@@ -60,22 +63,17 @@ export const createContactController = async (req, res) => {
 
 export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  // const result = await patchContact(contactId, req.user._id, {
-  //   ...req.body,
-  //   photo: photoUrl,
-  // });
   const photo = req.file;
 
   let photoUrl;
 
   if (photo) {
-    photoUrl = await saveFileToUploadDir(photo);
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
   }
-
-  // const result = await patchContact(contactId, {
-  //   ...req.body,
-  //   photo: photoUrl,
-  // });
 
   const result = await patchContact(contactId, req.user._id, {
     ...req.body,
